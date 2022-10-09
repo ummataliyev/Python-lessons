9d7e9fcc8034f63e8fcc2ca5268d86fcb5b8483f
ummataliyev <djohnummataliyev@gamil.com>
2022-10-09 16:48:11 +0500

    For Repetition has been Modified

--- for-repetition.py
+++ for-repetition.py
@@ -21,24 +21,3 @@ print("Write five your best friend's name:")
 for n in range (5):
     friends.append(input(f" {n+1}- write your friends name:"))
     print(friends)
-
-# Homework
-
-names = ["Umidjon", "Muhammadali", "Lily", "Sofia", "Moonlight"]
-n = len(names)
-for name in names:
-    print(f"Hello {name}")
-print(f"kod {n} marta aylandi")
-numbers = list(range(10,100))
-for number in numbers:
-    print(number ** 3)
-films = []
-print("Write your five favourite film?:")
-for n in range (5):
-    films.append(input(f" {n+1} - write your next film:"))
-    print(films)
-n_people = int(input("How many people do you meet today? :"))
-names = []
-for name in range(n_people):
-    name.append(input(f"{n+1}-who were they? :"))
-print(names)
