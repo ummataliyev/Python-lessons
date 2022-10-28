0b56dc1b263d7ee2eabb8e72c6d086a8792d1ce0
ummataliyev <djohnummataliyev@gmail.com>
2022-10-28 14:43:30 +0500

    Python lib

---
+++ main2.py
@@ -0,0 +1,21 @@
+import datetime as dt
+
+now = dt.datetime.today()
+
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.date())
+
+myDate = now.date()
+
+for i in range(14, 140, 14):
+   myDate = myDate + dt.timedelta(days=i)
+   print(myDate)
+
+
+birth = dt.date(2022, 10, 3)
+today = dt.date.today()
+difference = today - birth
+
+print(difference)
--- pythonlib.py
+++ pythonlib.py
@@ -91,3 +91,26 @@ from uzwords import words
 #     if re.match(andoza, word):
 #         matches.append(word)
 # print(matches)
+
+ # Homework
+import datetime as dt
+
+now = dt.datetime.today()
+
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.date())
+
+myDate = now.date()
+
+for i in range(14, 140, 14):
+   myDate = myDate + dt.timedelta(days=i)
+   print(myDate)
+
+
+birth = dt.date(2022, 10, 3)
+today = dt.date.today()
+difference = today - birth
+
+print(difference)
