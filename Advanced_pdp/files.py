dbd66c5c09f6e94a77f368799d11b8db4a4d4bbc
ummataliyev <djohnummataliyev@gmail.com>
2023-02-09 14:18:23 +0500

    Files

--- Advanced_pdp/files.py
+++ Advanced_pdp/files.py
@@ -1,18 +1,58 @@
+import json
+
 #Files
 
-file = open("word.txt")
-while True:
-    content = file.read(10)
-    if content == "":
-        break                               
-    print(content, end="")
+# file = open("word.txt")
+# while True:
+#     content = file.read(10)
+#     if content == "":
+#         break                               
+#     print(content, end="")
 
 
-file.close()
+# file.close()
 
-files = []
+# files = []
 
-for i in range(1000):
-    with open(f"trash/{i}.txt", "w") as file:
-        files.append(file)
+# for i in range(1000):
+#     with open(f"trash/{i}.txt", "w") as file:
+#         files.append(file)
 
+# #XMl
+# """
+# <document>
+#     <key>Value</key>
+#     <name>Value</name>
+# </document
+# """
+
+# #JSON
+# """
+# {
+#     "key": "value",
+#     "name": "John",
+#     "job": "programmer"
+# }
+# """
+
+# #CSV
+# """
+# key, name, job
+# value, John, programmer
+# value2, Bob, designer
+# """
+
+
+dictionary = {
+    "key": "value",
+    "name": "John",
+    "job": "programmer"
+}
+
+content = json.dumps(dictionary)
+with open("file.json", "w") as file:
+    file.write(content)
+
+with open("file.json", "r") as file:
+    dictionary = json.load(file)
+    print(type(dictionary))
