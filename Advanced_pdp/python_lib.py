853ab0188b38b4896eac07a9505cae2d659858bb
ummataliyev <djohnummataliyev@gmail.com>
2023-02-09 18:42:17 +0500

    Py Lib

---
+++ Advanced_pdp/python_lib.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+from collections import Counter
+
+
+yesterday = datetime.now() - timedelta(days=1)
+tomorrow = datetime.now() + timedelta(days=1)
+
+print(yesterday)
+print(tomorrows)
+
+from decimal import Decimal
+# working with moneys or float numbers
